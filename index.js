dcea5fe02eb2571476176712dc1ae048d9acb485
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-04-28 22:27:06 +0400

    connected with mongodb

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-const bookHandler = require('./routeHandler/bookHandler');
 require('dotenv').config();
 
 //middleware
@@ -9,7 +8,27 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
-app.use('/book', bookHandler);
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ztcrb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async() => {
+    try{
+         await client.connect();
+         const bookCollection = client.db('inventoryManagement').collection("book");
+            
+        })
+
+    }
+    catch(err){
+      console.log(err.message);
+    }
+}
+
+app.get('/', (req,res)=> {
+    res.send('hello!user');
+})
 
 app.listen(port, () => {
     console.log(`Listening to the port ${port}`);
